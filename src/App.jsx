453e190198673a3045f532e610ba7bfc217d37f3
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2025-01-23 15:44:05 +0100

    Create main App component in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// import { useState } from 'react';
+// import { CounterContext } from './contextAPI/context/counterContext';
+// import { Counter } from './contextAPI/Counter';
+
+// export default function App() {
+//   const [counter, setCounter] = useState(0);
+
+//   return (
+//     <CounterContext.Provider value={[counter, setCounter]}>
+//       <Counter />
+//     </CounterContext.Provider>
+//   );
+// }
+// import {Routes,Route} from 'react-router'
+// import Page1 from './react-routes/pages/page1'
+// import Page3 from './react-routes/pages/page3'
+// import Page2 from './react-routes/pages/page2'
+// import Home from './react-routes/pages/Home'
+// export default function App() {
+//   return(
+//     <Routes>
+//       <Route path='/' element={<Home />}></Route>
+//       <Route path='/route1' element={<Page1 />}></Route>
+//       <Route path='/route2' element={<Page2 />}></Route>
+//       <Route path='/route3' element={<Page3 />}></Route>
+//     </Routes>
+//   )
+
+// };
